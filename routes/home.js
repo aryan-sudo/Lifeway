cb8865d0082e5a592196bac304f5364ec6cfe454
Aryan <aryanp1.kvpattom@gmail.com>
2022-04-01 00:23:05 +0530

    route changes

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,7 @@ const session = require("express-session");
 const mongoose = require("mongoose");
 const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
+const Posts = require("../models/postModel");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
@@ -11,11 +12,25 @@ const LocalStrategy = require("passport-local").Strategy;
 const router = express.Router();
 
 router.get("/profile", async (req, res) => {
-    res.render("profile")
+    const see = await User.
+        findOne({ username: req.user }).
+        populate('posts').
+        exec(function (err, story) {
+            if (err) return handleError(err);
+            console.log('The author is %s', User.posts.title);
+            // prints "The author is Ian Fleming"
+        });
+
+    res.render("profile", { see })
 });
 
 router.get("/newsfeed", async (req, res) => {
+
     res.render("newsfeed", { user: req.user });
+});
+
+router.post("/profile", async (req, res) => {
+    res.render("profile", { user: req.user });
 });
 
 router.post('/signup', (req, res) => {
